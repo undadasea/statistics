d6fb7d7eca72aa0eb1181179a1775b9bedd4d28e
undadasea <valeriatazh@gmail.com>
2019-02-26 00:09:18 +0300

    three methods added no plots yet

--- lab2_geometric.py
+++ lab2_geometric.py
@@ -14,42 +14,90 @@ def get_p(p, r):
     if r == 1:
         return p
     else:
-        #return get_p(p, r-1)*(1-p)
-        return p*(1-p)**(r-1)
+        return get_p(p, r-1)*(1-p)
+
 
 def get_dense_p(p, r):
     return p*((1-p)**(r-1))
 
 
+def run_first_alg(p, num_loop):
+    array_random = np.zeros(num_loop)
+    for i in range(num_loop):
+        alpha = np.random.uniform()
+        r = 1
+        while alpha > 0:
+            alpha -= get_p(p, r)
+            r += 1
+
+        array_random[i] = r - 1
+    return array_random
+
+
+def run_second_alg(p, num_loop):
+    array_random = np.zeros(num_loop)
+    for i in range(num_loop):
+        u = np.random.uniform()
+        r = 0
+        while u > p:
+            r += 1
+            u = np.random.uniform()
+        array_random[i] = r
+    return array_random
+
+
+def run_third_alg(p, num_loop):
+    array_random = np.zeros(num_loop)
+    for i in range(num_loop):
+        u = np.random.uniform()
+        r = int(np.log(u)/np.log(1-p)) + 1
+        array_random[i] = r
+    return array_random
+
+
 p = 0.5
 
-array_geom_manual = np.zeros(10**5)
 
-for i in range(10**5):
-
-    alpha = np.random.uniform()
-    r = 1
-    while alpha > 0:
-        alpha -= get_p(p, r)
-        r += 1
-
-    array_geom_manual[i] = r-1
+array_geom_manual = run_first_alg(p, 10**5)
+array_geom_direct = run_second_alg(p, 10**6)
+array_geom_log = run_third_alg(p, 10**5)
 
 
 D_m = np.var(array_geom_manual)
 M_m = np.mean(array_geom_manual)
 
-print(D_m, M_m)
+D_d = np.var(array_geom_direct)
+M_d = np.mean(array_geom_direct)
+
+D_l = np.var(array_geom_log)
+M_l = np.mean(array_geom_log)
+
+print(M_m, D_m)
+print(M_d, D_d)
+print(M_l, D_l)
 
 
+###################################
 y = np.zeros(10)
 x = np.zeros(10)
 for i in range(10):
-    print(get_p(p, i+1))
+    #print(get_p(p, i+1))
     y[i] = get_p(p, i+1)
     x[i] = i
 plot_ = plt.subplot()
 plot_.plot(x, y)
 
 plt.show()
+##################################
+
+
+# test p and dense p
+array_dense = np.zeros(12)
+array_p = np.zeros(12)
+
+for k in range(len(array_dense)):
+    array_dense[k] = get_dense_p(p, k+1)
+    array_p[k] = get_p(p, k+1)
+
+
 
